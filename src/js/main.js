91e5f414d0713630f96e1045e7d1b15e61b8da4a
George Park <georgewpark@outlook.com>
2019-03-12 13:49:31 +0000

    refactor portfolio filter

--- src/js/main.js
+++ src/js/main.js
@@ -14,23 +14,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const filter = target.textContent.toLowerCase().replace(' ', '-');
 
-            //Remove styling for previously selected filter button
-            document.querySelector('.current').classList.remove('current');
-
-            //Add styling for currently selected filter button
-            target.classList.add('current');
-
             //Check new filter is not the current filter
             if (filter !== prevFilter) {
 
+                //Remove styling for previously selected filter button
+                document.querySelector('.current').classList.remove('current');
+
+                //Add styling for currently selected filter button
+                target.classList.add('current');
+
+                //Show/hide portfolio items
                 portfolioItems.forEach(item => {
 
                     const itemClasses = Array.from(item.classList);
 
+                    //Hide previous portfolio items
                     if (itemClasses.includes('show-item')) {
                         item.classList.remove('show-item');
                     }
 
+                    //Show new portfolio items
                     if (itemClasses.includes(filter)) {
                         setTimeout(() => item.classList.add('show-item'), 10);
                     }
