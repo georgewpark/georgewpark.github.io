9d6fc9479013aa4dd9f66dac6079e96da1451170
George Park <georgewpark@outlook.com>
2019-09-08 13:44:21 +0100

    refactor portfolio filter

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const portfolioFilter = document.querySelector('.portfolio-filter'),
-        portfolioItems = document.querySelectorAll('.portfolio-item');
+    const portfolioFilter = document.querySelector('.portfolio-filter');
+    const portfolioItems = document.querySelectorAll('.portfolio-item');
 
-    let prevFilter = 'featured';
+    let currentFilter = 'featured';
 
     portfolioFilter.addEventListener('click', e => {
 
         const target = e.target;
 
         //Check if filter button was pressed
-        if (Array.from(target.classList).includes('portfolio-filter__btn')) {
+        if (target.classList.contains('portfolio-filter__btn')) {
 
-            const filter = target.textContent.toLowerCase().replace(' ', '-');
+            const selectedFilter = target.textContent.toLowerCase().replace(' ', '-');
+            const currentFilterBtn = document.querySelector('.current');
 
-            //Check new filter is not the current filter
-            if (filter !== prevFilter) {
+            //Check selected filter is not the current filter
+            if (selectedFilter !== currentFilter) {
 
                 //Remove styling for previously selected filter button
-                document.querySelector('.current').classList.remove('current');
+                currentFilterBtn.classList.remove('current');
 
                 //Add styling for currently selected filter button
                 target.classList.add('current');
 
                 //Show/hide portfolio items
                 portfolioItems.forEach(item => {
-
-                    const itemClasses = Array.from(item.classList);
-
-                    //Hide previous portfolio items
-                    if (itemClasses.includes('show-item')) {
+                    if (item.classList.contains(selectedFilter)) {
+                        item.classList.add('show-item')
+                    } else {
                         item.classList.remove('show-item');
-                    }
-
-                    //Show new portfolio items
-                    if (itemClasses.includes(filter)) {
-                        setTimeout(() => item.classList.add('show-item'), 10);
                     }
                 });
 
                 //Update filter
-                prevFilter = filter;
+                currentFilter = selectedFilter;
             }
         }
     });
