a82214e7ebcf02898c03d999913a06f8a4c57be2
George Park <georgewpark@outlook.com>
2023-04-17 20:06:16 +0100

    add aria attributes to projects grid

--- src/js/components/ProjectFilters.tsx
+++ src/js/components/ProjectFilters.tsx
@@ -6,21 +6,24 @@ const ProjectFilters = () => {
   const { currentFilter, setCurrentFilter } = useContext(ProjectsContext)
 
   return (
-    <ul className='projects-filter'>
-      {
-        filterList.map(filter =>
-          <li>
-            <button
-              id={filter.toLowerCase().replace(' ', '-')}
-              className={`projects-filter__btn btn--dark ${filter === currentFilter ? 'current' : ''}`}
-              onClick={() => setCurrentFilter(filter)}
-            >
-              {filter}
-            </button>
-          </li>
-        )
-      }
-    </ul>
+    <nav aria-label='projects navigation'>
+      <ul className='projects-filter'>
+        {
+          filterList.map(filter =>
+            <li>
+              <button
+                id={filter.toLowerCase().replace(' ', '-')}
+                className={`projects-filter__btn btn--dark`}
+                aria-current={filter === currentFilter ? true : false}
+                onClick={() => setCurrentFilter(filter)}
+              >
+                {filter}
+              </button>
+            </li>
+          )
+        }
+      </ul>
+    </nav>
   )
 }
 
